a418aba1ecce28fdee538a8b61dc536d104e0f8d
wandahernandez13 <wandacarolina20@gmail.com>
2024-08-24 22:19:42 -0500

    Third commit
    Implementación de vistas de lista y detalle de publicaciones con paginación

--- mi_blog/blog/views.py
+++ mi_blog/blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Publicacion
+from .forms import PublicacionForm
+from django.db.models import Q
+from django.core.paginator import Paginator
 
-# Create your views here.
+def lista_publicaciones(request):
+    publicaciones = Publicacion.objects.all()
+    # Filtrado:
+    fecha_publicacion = request.GET.get('fecha_publicacion')
+    categoria = request.GET.get('categoria')
+    if fecha_publicacion:
+        publicaciones = publicaciones.filter(fecha_publicacion=fecha_publicacion)
+    if categoria:
+        publicaciones = publicaciones.filter(categoria__icontains=categoria)
+
+    # Búsqueda:
+    query = request.GET.get('q')
+    if query:
+        publicaciones = publicaciones.filter(
+            Q(titulo__icontains=query) | Q(contenido__icontains=query)
+        )
+    
+    # Paginación:
+    paginator = Paginator(publicaciones, 10)  # 10 publicaciones por página
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/lista_publicaciones.html', {'page_obj': page_obj})
+
+def detalle_publicacion(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/detalle_publicacion.html', {'publicacion': publicacion})
